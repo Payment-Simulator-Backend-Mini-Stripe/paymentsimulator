5d7947d2032187b5ef55d1c671df3d300731b9f8
joacoxmdb <joaquinpapagianacopoulos@gmail.com>
2026-02-20 23:34:46 -0300

    Fixed problems with db folder and reorganizate architecture

--- db/database.py
+++
@@ -1,9 +0,0 @@
-import os
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import declarative_base, sessionmaker
-
-DATABASE_URL = f"postgresql+asyncpg://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
-engine = create_async_engine(DATABASE_URL)
-Base = declarative_base()
-
-AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
