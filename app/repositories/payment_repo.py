0206f2bb0e7ab52c6684129bdcb2bcc67ec774ea
joacoxmdb <joaquinpapagianacopoulos@gmail.com>
2026-02-21 15:04:47 -0300

    feat: payment service added with status

--- app/repositories/payment_repo.py
+++ app/repositories/payment_repo.py
@@ -1,5 +1,7 @@
 from models.payment import Payment
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from app.schemas.payment import PaymentStatus
 
 
 class PaymentRepository:
@@ -11,3 +13,20 @@ class PaymentRepository:
         await self.session.commit()
         await self.session.refresh(payment_data)
         return payment_data
+    
+    async def get_payment_by_id(self, payment_id: int):
+        result = await self.session.execute(select(Payment).where(Payment.id == payment_id))
+        return result.scalars().first()
+    
+    async def get_all_payments(self, merchant_id: int):
+        result = await self.session.execute(select(Payment).where(Payment.merchant_id == merchant_id))
+        return result.scalars().all()
+    
+    async def update_payment_status(self, payment_id: int, new_status: PaymentStatus):
+        payment = await self.get_payment_by_id(payment_id)
+        if payment is None:
+            return None
+        payment.status = new_status
+        await self.session.commit()
+        await self.session.refresh(payment)
+        return payment
