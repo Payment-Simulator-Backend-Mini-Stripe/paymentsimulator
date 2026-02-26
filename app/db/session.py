86cc0c2f3e8af8f31746d228630ac27336be8775
Lucas Bigiatti <lucasbigiatti@gmail.com>
2026-02-26 00:16:06 -0300

    feat: alembic migrations created for merchants and payments

--- app/db/session.py
+++ app/db/session.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 from app.core.config import settings
 
+
 engine = create_async_engine(settings.DATABASE_URL)
 Base = declarative_base()
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession)
