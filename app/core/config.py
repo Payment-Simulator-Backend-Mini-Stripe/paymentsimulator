16d94353d438f72a7db841ac17993efedb4d7eaf
Lucas <lucasbigiatti@gmail.com>
2026-03-15 21:13:27 -0300

    feat: add pagination to payments history and update color theme

--- app/core/config.py
+++ app/core/config.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     POSTGRES_HOST: str
     POSTGRES_PORT: str
     MAX_PAYMENT_AMOUNT: int = 10000
-    MAX_ACTIVE_PAYMENTS_PER_MERCHANT: int = 5
+    MAX_ACTIVE_PAYMENTS_PER_MERCHANT: int = 100
 
     @property
     def DATABASE_URL(self) -> str:
